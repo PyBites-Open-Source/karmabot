99c168616605231f5275937eaea66b0d7bf21f32
Bob Belderbos <bobbelderbos@gmail.com>
2018-09-20 12:56:26 +0200

    need a strip on docstring

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,5 @@
 def create_commands_table(commands):
     """Print this help text"""
-    ret = '\n'.join(['{:<30}: {}'.format(name, func.__doc__)
+    ret = '\n'.join(['{:<30}: {}'.format(name, func.__doc__.strip())
                      for name, func in sorted(commands.items())])
     return '```{}```'.format(ret)
