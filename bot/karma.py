cd0b13e6d9bb2004a9514473f2fc73defae6b4b1
Derrick Walton <derrick@tosstalk.com>
2019-08-23 16:23:50 -0600

    fix small messaging issue.

--- bot/karma.py
+++ bot/karma.py
@@ -1,19 +1,15 @@
 from . import IS_USER, MAX_POINTS, karmas
 from .slack import lookup_username, post_msg
-
 KARMABOT = 'lord_commander'
 
 
 def _parse_karma_change(karma_change):
     userid, voting = karma_change
-
     if IS_USER.match(userid):
         receiver = lookup_username(userid)
     else:
         receiver = userid.strip(' #').lower()
-
     points = voting.count('+') - voting.count('-')
-
     return receiver, points
 
 
@@ -39,11 +35,11 @@ def process_karma_changes(message, karma_changes):
 
 
 class Karma:
-
     def __init__(self, giver, receiver):
         self.giver = giver
         self.receiver = receiver
         self.last_score_maxed_out = False
+
 
     def _calc_final_score(self, points):
         if abs(points) > MAX_POINTS:
@@ -52,6 +48,7 @@ class Karma:
         else:
             self.last_score_maxed_out = False
             return points
+
 
     def _create_msg_bot_self_karma(self, points):
         receiver_karma = karmas.get(self.receiver, 0)
@@ -65,19 +62,19 @@ class Karma:
             msg += ', I will work harder next time'
         return msg
 
+
     def _create_msg(self, points):
         poses = "'" if self.receiver.endswith('s') else "'s"
         action = 'increase' if points > 0 else 'decrease'
         receiver_karma = karmas.get(self.receiver, 0)
-
-        msg = '{}{} karma {}d to {}'.format(self.receiver,
+        msg = '@{}{} karma {}d to {}'.format(self.receiver,
                                             poses,
                                             action,
                                             receiver_karma)
         if self.last_score_maxed_out:
             msg += ' (= max {} of {})'.format(action, MAX_POINTS)
-
         return msg
+
 
     def change_karma(self, points):
         '''updates karmas dict and returns message string'''
@@ -86,14 +83,10 @@ class Karma:
                    'not be called with a non int for '
                    'points arg!')
             raise RuntimeError(err)
-
         if self.giver == self.receiver:
             raise ValueError('Giving yourself karma is stricky prohibited')
-
         points = self._calc_final_score(points)
-
         karmas[self.receiver] += points
-
         if self.receiver == KARMABOT:
             return self._create_msg_bot_self_karma(points)
         else:
