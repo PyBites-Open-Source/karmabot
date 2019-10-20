432c5c1b6379aef435c10a90c3efcbcabb487a3f
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-10-20 16:12:52 +0200

    moved connection check to slack.py for delayed execution via main

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,8 +4,6 @@ import re
 import sys
 
 from slackclient import SlackClient
-from bot.db import db_session
-
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -14,8 +12,8 @@ logging.basicConfig(
     filename="bot.log",
 )
 
-botuser_id = os.environ.get('SLACK_KARMA_BOTUSER')
-token = os.environ.get('SLACK_KARMA_TOKEN')
+botuser_id = os.environ.get("SLACK_KARMA_BOTUSER")
+token = os.environ.get("SLACK_KARMA_TOKEN")
 
 if not botuser_id or not token:
     print("Make sure you set SLACK_KARMA_BOTUSER and SLACK_KARMA_TOKEN in env")
@@ -23,15 +21,6 @@ if not botuser_id or not token:
 
 KARMABOT_ID = botuser_id
 SLACK_CLIENT = SlackClient(token)
-
-
-# Slack Real Time Messaging API - https://api.slack.com/rtm
-if not SLACK_CLIENT.rtm_connect():
-    logging.error("Connection Failed, invalid token?")
-    sys.exit(1)
-
-# Database
-db_session.global_init()
 
 # Regex
 # the first +/- is merely signaling, start counting (regex capture)
