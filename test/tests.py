77039403883b404123c063dbe28ddabb71da1a1e
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-10-20 19:01:51 +0200

    new tests and testdata for karmabot with db

--- test/tests.py
+++ test/tests.py
@@ -1,120 +1,142 @@
-import os
-from nose.tools import assert_raises
-from unittest.mock import patch
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from pathlib import Path
 
-from bot import SLACK_CLIENT, KARMA_BOT, KARMA_CACHE, MAX_POINTS
-from bot import KARMA_ACTION, USERNAME_CACHE, karmas
-from bot.slack import post_msg, parse_next_msg, lookup_username, Message
-from bot.karma import Karma, _parse_karma_change
+from bot import SLACK_CLIENT
+from bot.db.karma_user import KarmaUser
+from bot.slack import format_user_id, get_available_username
 
-userinfo = {
-    'user': {
-        'name': 'bob',
-    }
-}
+from test.slack_testdata import TEST_USERINFO
 
-# it's ok to remove the pickle file locally
-if os.path.isfile(KARMA_CACHE):
-    os.remove(KARMA_CACHE)
+# Database mocks
+@pytest.fixture(scope="session")
+def engine():
+    return create_engine(f"sqlite:///{Path().absolute() / 'karma_test.db'}")
 
 
-class TestKarma(object):
-    '''https://realpython.com/blog/python/testing-third-party-apis-with-mocks/'''
-
-    @classmethod
-    def setup_class(cls):
-        cls.mock_slack_api_call = patch('bot.SLACK_CLIENT.api_call', return_value=userinfo)
-        cls.mock_api_call = cls.mock_slack_api_call.start()
-        cls.mock_slack_rtm_read = patch('bot.SLACK_CLIENT.rtm_read')
-        cls.mock_rtm_read = cls.mock_slack_rtm_read.start()
-
-    @classmethod
-    def teardown_class(cls):
-        cls.mock_slack_api_call.stop()
-        cls.mock_slack_rtm_read.stop()
-
-    def test_lookup_username(self):
-        user = lookup_username('@bbelderbos')
-        assert user == 'bob'
-        assert 'bbelderbos' in USERNAME_CACHE
-        assert USERNAME_CACHE.get('bbelderbos') == 'bob'
-        assert len(USERNAME_CACHE) == 1
-        user = lookup_username('@bbelderbos')
-        assert len(USERNAME_CACHE) == 1
-        org_name = userinfo['user'].get('name')
-        userinfo['user']['name'] = 'tim'
-        user = lookup_username('@cook')
-        assert user == 'tim'
-        assert 'cook' in USERNAME_CACHE
-        assert USERNAME_CACHE.get('cook') == 'tim'
-        assert len(USERNAME_CACHE) == 2
-        user = lookup_username('@cook')
-        assert len(USERNAME_CACHE) == 2
-        userinfo['user']['name'] = org_name
+@pytest.fixture(scope="session")
+def tables(engine):
+    KarmaUser.metadata.create_all(engine)
+    yield
+    KarmaUser.metadata.drop_all(engine)
 
 
-    def test_parse_next_msg(self):
-        self.mock_rtm_read.return_value = None
-        assert parse_next_msg() == None
-        self.mock_rtm_read.return_value = [
-            {'user': 'U5Z6KGX4L'}  # karmabot self
-        ]
-        assert parse_next_msg() == None
-        channel, text = None, 'some text'
-        assert parse_next_msg() == None
-        channel, text = 'codechallenges', None
-        assert parse_next_msg() == None
-        channel, text, giverid = 'codechallenges', 'some new message', 'U5Z6ABCDE'
-        self.mock_rtm_read.return_value = [
-            {'user': giverid,
-            'channel': channel,
-            'text': text,}
-        ]
-        assert parse_next_msg() == Message(giverid=giverid, channel=channel, text=text)
+@pytest.fixture
+def empty_db_session(engine, tables):
+    """ Returns an SQLAlchemy session, and after the test
+    tears down everything properly.
+    """
+    connection = engine.connect()
+    # begin the nested transaction
+    transaction = connection.begin()
+    # use the connection with the already started transaction
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    # roll back the broader transaction
+    transaction.rollback()
+    # put back the connection to the connection pool
+    connection.close()
 
 
-    def test_parse_karma_change(self):
-        karma_change = '<@bbelderbos>', ' +++'
-        receiver, points = _parse_karma_change(karma_change)
-        assert receiver == 'bob'
-        assert points == 3
-        karma_change = 'subject', ' +-++'
-        receiver, points = _parse_karma_change(karma_change)
-        assert receiver == 'subject'
-        assert points == 2
-        karma_change = 'subject', ' ++---'
-        receiver, points = _parse_karma_change(karma_change)
-        assert points == -1
+@pytest.fixture
+def filled_db_session(engine, tables):
+    """ Returns an SQLAlchemy session, and after the test
+    tears down everything properly.
+    """
+    connection = engine.connect()
+    # begin the nested transaction
+    transaction = connection.begin()
+    # use the connection with the already started transaction
+    session = Session(bind=connection)
+
+    users = [
+        KarmaUser(user_id="ABC123", username="pybob", karma_points=392),
+        KarmaUser(user_id="EFG123", username="Julian Sequeira", karma_points=123),
+        KarmaUser(user_id="XYZ123", username="clamytoe", karma_points=420),
+    ]
+    session.bulk_save_objects(users)
+    session.commit()
+
+    yield session
+
+    session.close()
+    # roll back the broader transaction
+    transaction.rollback()
+    # put back the connection to the connection pool
+    connection.close()
 
 
-    def test_change_karma(self):
-        karma = Karma('bob', 'tim')
-        assert karma.change_karma(4) == "tim's karma increased to 4"
-        assert karmas.get('tim') == 4
-        assert karma.change_karma(2) == "tim's karma increased to 6"
-        assert karmas.get('tim') == 6
-        assert karma.change_karma(-1) == "tim's karma decreased to 5"
-        assert karmas.get('tim') == 5
-        msg = "tim's karma increased to 10 (= max increase of {})".format(MAX_POINTS)
-        assert karma.change_karma(MAX_POINTS + 3) == msg
-        assert karmas.get('tim') == 5 + MAX_POINTS
-        msg = "tim's karma decreased to 5 (= max decrease of {})".format(MAX_POINTS)
-        prev_score = karmas.get('tim')  # 10
-        assert karma.change_karma(-12) == msg
-        assert karmas.get('tim') == prev_score - MAX_POINTS  # 5
+# Slack API mocks
+@pytest.fixture
+def mock_slack_rtm_read(monkeypatch):
+    def mock_rtm_read(*args, **kwargs):
+        return {"type": "message", "user": "ABC123", "text": "Hi everybody"}
+
+    monkeypatch.setattr(SLACK_CLIENT, "rtm_read", mock_rtm_read)
 
 
-    def test_change_karma_exceptions(self):
-        karma = Karma('bob', 'tim')
-        assert_raises(RuntimeError, karma.change_karma, 'string')
-        karma = Karma('bob', 'bob')
-        assert_raises(ValueError, karma.change_karma, 1)
+@pytest.fixture
+def mock_slack_api_call(monkeypatch):
+    def mock_api_call(*args, **kwargs):
+        if args[0] == "users.info":
+            user_id = kwargs.get("user")
+            return TEST_USERINFO[user_id]
+
+    monkeypatch.setattr(SLACK_CLIENT, "api_call", mock_api_call)
 
 
-    def test_change_karma_bot_self(self):
-        karma = Karma('bob', 'karmabot')
-        assert karma.change_karma(2) == "Thanks @bob for the extra karma, my karma is 2 now"
-        karma = Karma('tim', 'karmabot')
-        assert karma.change_karma(3) == "Thanks @tim for the extra karma, my karma is 5 now"
-        karma = Karma('julian', 'karmabot')
-        assert karma.change_karma(-3) == "Not cool @julian lowering my karma to 2, but you are probably right, I will work harder next time"
+# Testing
+def test_slack_rtm_read(mock_slack_rtm_read):
+    event = SLACK_CLIENT.rtm_read()
+    assert event["type"] == "message"
+    assert event["user"] == "ABC123"
+    assert event["text"] == "Hi everybody"
+
+
+@pytest.mark.parametrize(
+    "test_user_id, expected",
+    [("ABC123", "pybob"), ("EFG123", "Julian Sequeira"), ("XYZ123", "clamytoe")],
+)
+def test_lookup_username(filled_db_session, test_user_id, expected):
+    karma_user = filled_db_session.query(KarmaUser).get(test_user_id)
+    assert karma_user.username == expected
+
+
+def test_parse_next_msg():
+    pass
+
+
+def test_parse_karma_change():
+    pass
+
+
+def test_change_karma():
+    pass
+
+
+def test_change_karma_exceptions():
+    pass
+
+
+def test_change_karma_bot_self():
+    pass
+
+
+@pytest.mark.parametrize(
+    "test_user_id, expected", [("ABC123", "<@ABC123>"), ("<@ABC123>", "<@ABC123>")]
+)
+def test_format_user_id(test_user_id, expected):
+    assert format_user_id(test_user_id) == expected
+
+
+@pytest.mark.parametrize(
+    "test_user_id, expected",
+    [("ABC123", "pybob"), ("EFG123", "Julian Sequeira"), ("XYZ123", "clamytoe")],
+)
+def test_get_available_username(mock_slack_api_call, test_user_id, expected):
+    user_info = SLACK_CLIENT.api_call("users.info", user=test_user_id)
+    assert get_available_username(user_info) == expected
