66cae05db270cc0ad0f3f2a7db659c2d4886e680
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-10-20 16:11:36 +0200

    added command to get current username

--- commands/update_username.py
+++ commands/update_username.py
@@ -1,6 +1,7 @@
+import bot
+import bot.slack
 from bot.db.db_session import create_session
 from bot.db.karma_user import KarmaUser
-import bot.slack
 
 
 def update_username(**kwargs):
@@ -14,7 +15,7 @@ def update_username(**kwargs):
         return "User not found"
 
     old_username = karma_user.username
-    user_info = bot.slack.SLACK_CLIENT.api_call("users.info", user=user_id)
+    user_info = bot.SLACK_CLIENT.api_call("users.info", user=user_id)
     new_username = bot.slack.get_available_username(user_info)
 
     if old_username == new_username:
@@ -32,3 +33,18 @@ def update_username(**kwargs):
         f"Sucessfully updated your KarmaUser name "
         f"from '{old_username}' to '{new_username}'!"
     )
+
+
+def get_user_name(**kwargs):
+    """Shows the current Username"""
+    user_id = kwargs.get("user_id").strip("<>@")
+
+    session = create_session()
+    karma_user: KarmaUser = session.query(KarmaUser).get(user_id)
+
+    if not karma_user:
+        return "Sorry, you are not yet known to karmabot. Try to give some Karma! :)"
+    username = karma_user.username
+    session.close()
+
+    return f"Your current username for karmabot is '{username}'"
