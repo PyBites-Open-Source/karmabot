775fce0313f3a3bb8e4e3fea7f53d7fd46ea7c7a
AJ Kerrigan <aj@speckledmonkey.com>
2020-01-01 20:02:18 -0500

    Fix minor typo, remove unused constant
    Remove the TOP_CHANNELS constant which is defined but never used.

--- commands/topchannels.py
+++ commands/topchannels.py
@@ -11,17 +11,7 @@ from slackclient import SlackClient
 
 from bot.settings import SLACK_CLIENT
 
-TOP_CHANNELS = """Glad you asked, here are some channels our Communtiy recommends:
-- #100daysofcode: share your 100 days journey and/or feedback on our course
-- #books: get notified about Packt's daily free ebook and/or share interesting books you are reading
-- #codechallenges: stuck? Ask your Python coding questions here, we learn more together!
-- #instagram: submit inspiring Python/coding/developer pictures for PyBites Instagram
-- #meetups / #pycon: the PyBites community is growing so maybe you can meet fellow Pythonistas in person!
-- #pybites-news: share interesting Python news/articles here helping us with our weekly Twitter news digest
-- #checkins: share your pythonic adventures with the community and help everyone grow!
-"""
-
-MSG_BEGIN = "Glad you asked, here are some channels our Communtiy recommends (based on member count and activity):\n"
+MSG_BEGIN = "Glad you asked, here are some channels our Community recommends (based on member count and activity):\n"
 MSG_LINE = (
     "- #{channel} ({member_count} members, last post {time_since_last_post}): {purpose}"
 )
