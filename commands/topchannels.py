781830143d070f9282ac6cb097b591b4b7ddd7c4
AJ Kerrigan <aj@speckledmonkey.com>
2020-01-02 04:16:22 -0500

    Humanize topchannels times (#33)
    * Humanize the time since last post for topchannels
    
    * Fix minor typo, remove unused constant
    
    Remove the TOP_CHANNELS constant which is defined but never used.

--- commands/topchannels.py
+++ commands/topchannels.py
@@ -6,22 +6,15 @@ from math import exp
 from operator import itemgetter
 from typing import Dict, List, Optional
 
+import humanize
 from slackclient import SlackClient
 
 from bot.settings import SLACK_CLIENT
 
-TOP_CHANNELS = """Glad you asked, here are some channels our Communtiy recommends:
-- #100daysofcode: share your 100 days journey and/or feedback on our course
-- #books: get notified about Packt's daily free ebook and/or share interesting books you are reading
-- #codechallenges: stuck? Ask your Python coding questions here, we learn more together!
-- #instagram: submit inspiring Python/coding/developer pictures for PyBites Instagram
-- #meetups / #pycon: the PyBites community is growing so maybe you can meet fellow Pythonistas in person!
-- #pybites-news: share interesting Python news/articles here helping us with our weekly Twitter news digest
-- #checkins: share your pythonic adventures with the community and help everyone grow!
-"""
-
-MSG_BEGIN = "Glad you asked, here are some channels our Communtiy recommends (based on member count and activity):\n"
-MSG_LINE = "- #{channel} ({member_count} members, {hours_since_last_post:.0f} hour(s) since last post): {purpose}"
+MSG_BEGIN = "Glad you asked, here are some channels our Community recommends (based on member count and activity):\n"
+MSG_LINE = (
+    "- #{channel} ({member_count} members, last post {time_since_last_post}): {purpose}"
+)
 DEFAULT_NR_CHANNELS = 7
 
 Channel = namedtuple("Channel", "id name purpose num_members latest_ts latest_subtype")
@@ -97,7 +90,9 @@ def get_recommended_channels(**kwargs):
             MSG_LINE.format(
                 channel=channel.name,
                 member_count=channel.num_members,
-                hours_since_last_post=seconds_since_last_post(channel) / 3600,
+                time_since_last_post=humanize.naturaltime(
+                    seconds_since_last_post(channel)
+                ),
                 purpose=channel.purpose
                 or "<Invest today and get an awesome description!>",
             )
