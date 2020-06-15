b14f350e0ee2c29a5d37eefd2d5d17a84a19279f
Patrick-Oliver Groß <mail@pogross.de>
2020-06-15 23:33:13 +0200

    deleted atm unsued files

--- run.sh
+++
@@ -1,6 +0,0 @@
-python=$(which python3)
-cmd="$python main.py"
-until $cmd; do
-    echo "Slack bot crashed with exit code $?. Respawning.." >&2
-    sleep 1
-done
--- src/karmabot/bot/db/modelbase.py
+++
@@ -1,4 +0,0 @@
-import sqlalchemy.ext.declarative as dec
-
-# Creates a base model you can derive model classes from
-SqlAlchemyBase = dec.declarative_base()
