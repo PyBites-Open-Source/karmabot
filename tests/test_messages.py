b65016c3fdd5bddc4e9ab7fe876bed37310504d8
Ivan Gonzalez <scratchmex@gmail.com>
2020-10-03 18:57:23 -0500

    ci(tests): removed empty tests

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -4,22 +4,6 @@ import pytest
 from karmabot.slack import perform_text_replacements
 
 
-def test_get_cmd():
-    pass
-
-
-def test_perform_bot_cmd():
-    pass
-
-
-def test_parse_next_msg():
-    pass
-
-
-def test_create_help_msg():
-    pass
-
-
 @pytest.mark.parametrize(
     "test_text, expected", [("Cheers everybody", "To _cheers_ I say: :beers:")]
 )
