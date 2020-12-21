bb0a1096b76b4698fa06d7092007940602f460c1
Patrick-Oliver Groß <mail@pogross.de>
2020-12-21 22:31:06 +0100

    fixed karma multiline issue #72

--- tests/test_karma.py
+++ tests/test_karma.py
@@ -3,10 +3,27 @@ import pytest
 from karmabot.db import db_session
 from karmabot.db.karma_user import KarmaUser
 from karmabot.karma import Karma, _parse_karma_change
-from karmabot.settings import KARMABOT_ID
+from karmabot.settings import KARMA_ACTION, KARMABOT_ID
 
 
 # Karma
+@pytest.mark.parametrize(
+    "test_message, expected",
+    [
+        ("<@ABC123> +++", ("<@ABC123>", "++")),
+        ("Some cool text\nAnother line\n <@ABC123> +++", ("<@ABC123>", "++")),
+        ("<@FOO42> ++++", ("<@FOO42>", "+++")),
+        ("First line\n <@BAR789> ++++\n some more text after", ("<@BAR789>", "+++")),
+    ],
+)
+def test_karma_regex(test_message, expected):
+    karma_changes = KARMA_ACTION.findall(test_message)
+    user_id, voting = karma_changes[0]
+
+    assert user_id == expected[0]
+    assert voting == expected[1]
+
+
 @pytest.mark.parametrize(
     "test_change, expected",
     [(("<@ABC123>", "+++"), ("ABC123", 3)), (("<@XYZ123>", "----"), ("XYZ123", -4))],
