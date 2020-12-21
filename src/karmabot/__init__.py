8e910867ecafe2cc6919d290d0b6d069dbe016f6
Patrick-Oliver Groß <mail@pogross.de>
2020-12-21 22:29:43 +0100

    formatting

--- src/karmabot/__init__.py
+++ src/karmabot/__init__.py
@@ -1,8 +1,8 @@
 """The PyBites Karmabot for Slack."""
 try:
-    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+    from importlib.metadata import PackageNotFoundError, version  # type: ignore
 except ImportError:  # pragma: no cover
-    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
 
 
 try:
