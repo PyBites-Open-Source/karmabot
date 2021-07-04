6bac20ec1d1cadd302149365be968e9ded5a34fa
Patrick-Oliver Groß <mail@pogross.de>
2021-07-04 21:32:52 +0200

    changes for new api package

--- src/karmabot/main.py
+++ src/karmabot/main.py
@@ -1,14 +1,14 @@
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from karmabot.bot import app
 from karmabot.db import db_session
 from karmabot.settings import SLACK_APP_TOKEN
-from karmabot.slack import bot
 
 
 def main():
     db_session.global_init()
 
-    handler = SocketModeHandler(bot, SLACK_APP_TOKEN)
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
 
 
