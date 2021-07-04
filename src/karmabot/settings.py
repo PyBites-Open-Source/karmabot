763939945621dc6891a6bdb3affad6f4e5c88a2f
Patrick-Oliver Groß <mail@pogross.de>
2021-07-04 21:32:47 +0200

    changes for new api package

--- src/karmabot/settings.py
+++ src/karmabot/settings.py
@@ -31,24 +31,17 @@ if dotenv_path.exists():
 # Environment variables
 KARMABOT_ID = _get_env_var("KARMABOT_SLACK_USER")
 DATABASE_URL = _get_env_var("KARMABOT_DATABASE_URL")
-
-# new stuff
 SLACK_APP_TOKEN = _get_env_var("KARMABOT_SLACK_APP_TOKEN")
 SLACK_BOT_TOKEN = _get_env_var("KARMABOT_SLACK_BOT_TOKEN")
-
-# Old stuff
-SLACK_TOKEN = None
-SLACK_INVITE_TOKEN = None
-SLACK_CLIENT = None
 
 # Slack
 GENERAL_CHANNEL = _get_env_var("KARMABOT_GENERAL_CHANNEL")
 ADMINS = _get_env_var("KARMABOT_ADMINS")
 ADMINS = ADMINS.split(",")  # type: ignore
-SLACK_ID_FORMAT = re.compile(r"^<@[^>]+>$")
+
 
 # Karma
 # the first +/- is merely signaling, start counting (regex capture)
 # from second +/- onwards, so bob++ adds 1 point, bob+++ = +2, etc
-KARMA_ACTION = re.compile(r"(?:^| )(\S{2,}?)\s?[\+\-]([\+\-]+)", flags=re.MULTILINE)
+KARMA_ACTION_PATTERN = re.compile(r"(?:^| )(\S{2,}?)\s?[\+\-]([\+\-]+)", flags=re.MULTILINE)
 MAX_POINTS = 5
