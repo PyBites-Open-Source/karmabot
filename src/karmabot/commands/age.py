bfe83b0daf4a2385d8340e18e63cd4b23e8a69e9
Patrick-Oliver Groß <mail@pogross.de>
2021-07-06 17:08:52 +0200

    removed unecessary main

--- src/karmabot/commands/age.py
+++ src/karmabot/commands/age.py
@@ -8,11 +8,3 @@ def pybites_age(**kwargs) -> str:
     today = datetime.now()
     days_old = (today - PYBITES_BORN).days
     return f"PyBites is {days_old} days old"
-
-
-if __name__ == "__main__":
-    # standalone test
-    user, channel, text = "bob", "#general", "some message"
-    kwargs = {"user": user, "channel": channel, "text": text}
-    output = pybites_age(**kwargs)
-    print(output)
