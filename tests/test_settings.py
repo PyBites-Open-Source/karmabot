86c7c3bae7014a2a53a1c134b35131e5a3535440
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-08 11:26:59 +0200

    fix import

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,27 +3,27 @@ from unittest.mock import patch
 
 import pytest
 
-from karmabot.settings import _get_env_var
+from karmabot.settings import get_env_var
 
 
 @pytest.mark.parametrize("env_var", ["true", "false"])
 def test_get_env_var(env_var):
     with patch.dict(os.environ, {"KARMABOT_TEST_MODE": env_var}):
-        assert _get_env_var("KARMABOT_TEST_MODE") == env_var
+        assert get_env_var("KARMABOT_TEST_MODE") == env_var
 
 
 def test_exception_env_variable_not_found():
     with patch.dict(os.environ, {}, clear=True):
         with pytest.raises(KeyError):
-            _get_env_var("KARMABOT_TEST_MODE")
+            get_env_var("KARMABOT_TEST_MODE")
 
 
 def test_exception_env_variable_empty():
     with patch.dict(os.environ, {"KARMABOT_TEST_MODE": ""}):
         with pytest.raises(ValueError):
-            _get_env_var("KARMABOT_TEST_MODE")
+            get_env_var("KARMABOT_TEST_MODE")
 
 
 def test_return_default_value_if_env_variable_not_set():
     with patch.dict(os.environ, {}, clear=True):
-        assert _get_env_var("KARMABOT_TEST_MODE", default="false") == "false"
+        assert get_env_var("KARMABOT_TEST_MODE", default="false") == "false"
